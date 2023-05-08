6639cd2eb89e590896ba28d24a102c21e19a1d53
Tom Faulkner <tomfaulkner@gmail.com>
2023-05-08 15:34:05 -0500

    provide tx

--- src/wat/views/manual_exec.py
+++ src/wat/views/manual_exec.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from ..lib import depends
 from ..svc import workflow
 
 router = APIRouter()
 
 
 @router.post("/manual_exec")
-async def manual_exec(wf_id: str):
-    return await workflow.execute_workflow(wf_id, suppress_updates=True)
+async def manual_exec(wf_id: str, tx=Depends(depends.edge_tx)):
+    return await workflow.execute_workflow(wf_id, suppress_updates=True, tx=tx)
 
 
 def init_app(app):
